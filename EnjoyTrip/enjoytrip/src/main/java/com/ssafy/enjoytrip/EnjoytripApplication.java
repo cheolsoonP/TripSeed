5a83c5a9dce70321dbc3dc8ee5817447c1e39b3e
박철순 <mn5724@naver.com>
2023-05-01 16:44:37 +0900

    feat: swagger setting #3

--- EnjoyTrip/enjoytrip/src/main/java/com/ssafy/enjoytrip/EnjoytripApplication.java
+++ EnjoyTrip/enjoytrip/src/main/java/com/ssafy/enjoytrip/EnjoytripApplication.java
@@ -2,8 +2,10 @@ package com.ssafy.enjoytrip;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.ssafy.enjoytrip"})
 public class EnjoytripApplication {
 
 	public static void main(String[] args) {

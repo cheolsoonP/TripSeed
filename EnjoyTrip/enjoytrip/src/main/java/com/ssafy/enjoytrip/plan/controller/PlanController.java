ffb69fac48cdd562c199cc6c25ea0aacb0fbfc39
박철순 <mn5724@naver.com>
2023-05-03 17:21:53 +0900

    feat: 여행 계획 junit 테스트

--- EnjoyTrip/enjoytrip/src/main/java/com/ssafy/enjoytrip/plan/controller/PlanController.java
+++ EnjoyTrip/enjoytrip/src/main/java/com/ssafy/enjoytrip/plan/controller/PlanController.java
@@ -1,11 +1,7 @@
 package com.ssafy.enjoytrip.plan.controller;
 
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-
-import javax.servlet.http.HttpSession;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;

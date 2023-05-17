4d7464946774d4a9180dd77481a40887e1061674
박철순 <mn5724@naver.com>
2023-05-17 15:58:50 +0900

    feat: board side bar

--- front/enjoytrip-front/src/store/index.js
+++ front/enjoytrip-front/src/store/index.js
@@ -1,6 +1,6 @@
 import Vue from "vue";
 import Vuex from "vuex";
-// import createPersistedState from "vuex-persistedstate";
+import createPersistedState from "vuex-persistedstate";
 // module import
 import attractionStore from "@/store/modules/attractionStore";
 import boardStore from "@/store/modules/boardStore";
@@ -19,9 +19,9 @@ export default new Vuex.Store({
     userStore,
   },
   plugins: [
-    // createPersistedState({
-    //   // 브라우저 종료시 제거하기 위해 localStorage가 아닌 sessionStorage로 변경. (default: localStorage)
-    //   storage: sessionStorage,
-    // }),
+    createPersistedState({
+      // 브라우저 종료시 제거하기 위해 localStorage가 아닌 sessionStorage로 변경. (default: localStorage)
+      storage: sessionStorage,
+    }),
   ],
 });
---
+++ front/enjoytrip-front/src/views/BoardView.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="board">
+    <side-bar></side-bar>
+  </div>
+</template>
+<script>
+import sideBar from "@/components/bar/side-bar.vue"
+import { mapActions, mapState } from "vuex"
+
+const regionStore = "regionStore";
+
+export default {
+    name: 'BoardView',
+  components: { sideBar },
+  data() {
+    return {
+
+    };
+  },
+  computed: {
+    ...mapState(regionStore, ["sidos"]),
+  },
+  created() {
+    this.getSido();
+  },
+  methods: {
+    ...mapActions(regionStore, ["getSido"]),
+    }
+  }
+</script>

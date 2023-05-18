d15df065e05f65fc8acaf37ffbec9b546e58e551
yeonuy <jyeonuy@gmail.com>
2023-05-19 05:21:08 +0900

    feat: 게시판 글쓰기 UI (미완성)

---
+++ front/enjoytrip-front/src/components/board/board-write.vue
@@ -0,0 +1,56 @@
+<template>
+  <v-form style="background-color: pink">
+    <v-row>
+      <v-col>
+        <v-autocomplete
+          v-model="selected"
+          :items="['Trevor Handsen', 'Alex Nelson']"
+          label="시도"
+          full-width
+          hide-details
+          hide-no-data
+          hide-selected
+          single-line
+        ></v-autocomplete>
+      </v-col>
+      <v-col>
+        <v-autocomplete
+          v-model="selected"
+          :items="['Trevor Handsen', 'Alex Nelson']"
+          label="군구"
+          full-width
+          hide-details
+          hide-no-data
+          hide-selected
+          single-line
+        ></v-autocomplete>
+      </v-col>
+    </v-row>
+
+    <v-divider></v-divider>
+    <v-text-field
+      placeholder="제목을 입력하세요"
+      single-line
+      full-width
+      hide-details
+    ></v-text-field>
+    <v-divider></v-divider>
+    <v-textarea
+      placeholder="내용을 입력하세요"
+      v-model="title"
+      label="Message"
+      counter
+      full-width
+      single-line
+    ></v-textarea>
+  </v-form>
+</template>
+
+<script>
+export default {
+  name: "BoardWrite",
+};
+</script>
+
+<style>
+</style>

4230c38a78a9ac49b040484309adc8cd4a2877db
박철순 <mn5724@naver.com>
2023-05-18 21:16:34 +0900

    fix: remove about view #51

--- front/enjoytrip-front/src/router/index.js
+++ front/enjoytrip-front/src/router/index.js
@@ -13,11 +13,6 @@ const routes = [
     component: HomeView,
   },
   {
-    path: "/about",
-    name: "about",
-    component: () => import("../views/AboutView.vue"),
-  },
-  {
     path: "/board",
     name: "board",
     component: () => import("@/views/BoardView"),

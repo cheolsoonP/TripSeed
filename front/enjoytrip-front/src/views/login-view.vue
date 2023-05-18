e47529ac9cffbb43f401e022596c774d585b3164
박철순 <mn5724@naver.com>
2023-05-18 21:19:36 +0900

    fix: basic home component 삭제 #51

---
+++ front/enjoytrip-front/src/views/login-view.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="login">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "LoginView",
+  
+}
+</script>
+
+<style>
+
+</style>

0727d30c2d185ef3e44a397d4e228877d827c995
박철순 <mn5724@naver.com>
2023-05-18 17:50:19 +0900

    feat: 게시판 세부페이지 조정

--- front/enjoytrip-front/src/components/board/board-detail.vue
+++ front/enjoytrip-front/src/components/board/board-detail.vue
@@ -1,25 +1,65 @@
 <template>
-  <div>
+  <v-container class="ma-10 d-flex justify-center" >
+    <div>
+      <div>
+        <v-col class="d-flex justify-center">{{ post.sidoName }} > {{ post.gugunName }}</v-col>
+          <v-col class="d-flex justify-center text-h4">{{ post.title }}</v-col>
+        <v-row
+          no-gutters
+        >
+          <v-col cols="2">{{ post.writerId }}</v-col>
+          <v-col>{{ post.writeDate }}</v-col>
+        </v-row>
+        <v-row class="d-flex justify-end">
+          <v-col cols="3">좋아요 {{ post.likeCount }}</v-col>
+          <v-col cols="3">조회수 {{ post.viewCount }}</v-col>
+        </v-row>
+      </div>
+      <div>
+        <v-col cols="12" class="my-10" >{{ post.content }}</v-col>
+      </div>
+      <div> 
+        <v-row>
+          <v-col class="d-flex justify-end"><v-btn color="primary">수정하기</v-btn></v-col>
+        </v-row>
+      </div>
+      <div>
+        <v-col class="my-n5">댓글 {{ post.commentCount }}</v-col>
+        <v-divider class="my-5"/>
+        <div>
+          <v-card
+            class="mx-auto my-4"
+            width="100%"
+            outlined
+          >
+            <v-list-item three-line>
+              <v-list-item-content>
+                <div class="text-overline mb-4">
+                  2022.05.21 12:24
+                </div>
+                <v-list-item-title class="text-h6 mb-1">
+                  김싸피
+                </v-list-item-title>
+                <v-list-item-content class="subtitle-2">댓글 내용입니다. 이글 너무 좋아요</v-list-item-content>
+              </v-list-item-content>
+            </v-list-item>
 
-    <v-container >
-      <!-- github-markdown-light -->
+            <v-card-actions>
+              <v-btn
+                outlined
+                rounded
+                text
+                color="primary"
+              >
+                답글
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+        </div>
+    </div>      
 
-      <v-row>{{ post.sidoName }} > {{ post.gugunName }}</v-row>
-      <v-row>{{ post.title }}</v-row>
-      <v-row>
-        <v-col>{{ post.writerId }}</v-col>
-        <v-col>{{ post.writerDate }}</v-col>
-      </v-row>
-      <v-row>
-        <v-col>좋아요 {{ post.likeCount }}</v-col>
-        <v-col>조회수 {{ post.viewCount }}</v-col>
-      </v-row>
-      
-      <v-row>{{ post.content }}</v-row>
-
-
-    </v-container>
-  </div>
+    </div>
+  </v-container>
 </template>
 
 <script>

af930d980621b75c217b91eeee303c12ada006f4
유연정 <jyeonuy@gmail.com>
2023-05-19 10:51:37 +0900

    feat: 헤더 컴포넌트 구현

---
+++ front/enjoytrip-front/src/components/bar/header-bar.vue
@@ -0,0 +1,42 @@
+<template>
+  <v-container>
+    <v-card elevation="0" color="#a0ffd6" class="my-4">
+      <v-row class="px-4">
+        <v-col>
+          <v-card tile elevation="0" color="transparent">
+            <v-col class="text-h5 text--primary pb-0"> {{ title }} </v-col>
+            <v-col>
+              <div class="text-h7 text--primary" v-html="content"></div>
+            </v-col>
+
+            <v-card-actions>
+              <v-btn elevation="0" color="primary" :to="url">
+                <v-icon>mdi-plus</v-icon>
+                <span class="pl-1">{{ action }}</span>
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-col>
+        <v-col cols="auto" class="d-flex justify-center">
+          <img :src="imgsrc" />
+        </v-col>
+      </v-row>
+    </v-card>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "HeaderBar",
+  components: {},
+  props: {
+    title: String,
+    content: String,
+    url: String,
+    action: String ,
+    imgsrc: String, 
+  },
+};
+</script>
+
+<style></style>

e355aa874e5203f1539daed29e8dceb988dbb771
박철순 <mn5724@naver.com>
2023-05-19 10:19:00 +0900

    fix: user controller crossorigin #54

--- back/enjoytrip/src/main/java/com/ssafy/enjoytrip/auth/controller/UserController.java
+++ back/enjoytrip/src/main/java/com/ssafy/enjoytrip/auth/controller/UserController.java
@@ -2,6 +2,7 @@ package com.ssafy.enjoytrip.auth.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,6 +19,7 @@ import lombok.RequiredArgsConstructor;
 @RestController
 @RequestMapping("/users")
 @RequiredArgsConstructor
+@CrossOrigin("*")
 public class UserController {
 	
 	private final UserService userService;

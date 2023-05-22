2418d8690609d9f9d993110353c3ce8fb079c3b5
박철순 <mn5724@naver.com>
2023-05-22 10:22:44 +0900

    feat: plan list Header bar #65

--- front/enjoytrip-front/src/components/plan/plan-list.vue
+++ front/enjoytrip-front/src/components/plan/plan-list.vue
@@ -1,7 +1,18 @@
 <template>
   <v-container>
+    <div>
+      <header-bar
+        title = "나만의 여행 계획"
+        content = "나만의 여행 계획을 세워보세요.<br/>친구를 초대해 일정을 공유할 수 있어요."
+        url="/plan/write"
+        btn_icon="mdi-plus"
+        btn_text="일정 생성"
+        imgsrc=""
+      />
+    </div>
     <v-row>
-      <!-- <v-col cols="3"><plan-side-bar/></v-col> -->
+    
+      <v-col cols="3"><plan-side-bar/></v-col>
       <v-col cols="9">
         <v-list>
           <v-list-item v-for="planInfo, index in plans" :key="index">
@@ -17,11 +28,12 @@
 // import PlanSideBar from "@/components/bar/plan-side-bar"
 import { mapActions, mapState } from "vuex"
 import PlanListItem from "@/components/plan/plan-list-item"
+import HeaderBar from "@/components/bar/header-bar.vue";
 const planStore = "planStore";
 const userStore = "userStore";
 
 export default {
-  components: { PlanListItem },
+  components: { PlanListItem, HeaderBar },
   data() {
     return {
 

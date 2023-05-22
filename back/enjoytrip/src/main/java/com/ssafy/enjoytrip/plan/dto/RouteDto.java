051ebfe242e1590dafe569cd241f3f44d0252986
박철순 <mn5724@naver.com>
2023-05-22 14:06:58 +0900

    fix: 여행 경로 불러오기시 관광지명, 주소 모두 가져오기 #65

--- back/enjoytrip/src/main/java/com/ssafy/enjoytrip/plan/dto/RouteDto.java
+++ back/enjoytrip/src/main/java/com/ssafy/enjoytrip/plan/dto/RouteDto.java
@@ -1,46 +1,23 @@
 package com.ssafy.enjoytrip.plan.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 public class RouteDto {
 	private int planId;
 	private int attractionId;
 	private int visitOrder;
 	private String memo;
 	private String visitTime;
-	public int getPlanId() {
-		return planId;
-	}
-	public void setPlanId(int planId) {
-		this.planId = planId;
-	}
-	public int getAttractionId() {
-		return attractionId;
-	}
-	public void setAttractionId(int attractionId) {
-		this.attractionId = attractionId;
-	}
-	public int getOrder() {
-		return visitOrder;
-	}
-	public void setOrder(int visitOrder) {
-		this.visitOrder = visitOrder;
-	}
-	public String getMemo() {
-		return memo;
-	}
-	public void setMemo(String memo) {
-		this.memo = memo;
-	}
-	public String getVisitTime() {
-		return visitTime;
-	}
-	public void setVisitTime(String visitTime) {
-		this.visitTime = visitTime;
-	}
-	@Override
-	public String toString() {
-		return "RouteDto [planId=" + planId + ", attractionId=" + attractionId + ", visitOrder=" + visitOrder + ", memo=" + memo
-				+ ", visitTime=" + visitTime + "]";
-	}
-	
-	
+	private String title;
+	private String latitude;
+	private String longitude;
+	private String image;
+	private String addr;
 }

dc740795f003c21ac8895149f66ad727da8fbc00
박철순 <mn5724@naver.com>
2023-05-25 09:37:13 +0900

    fix: join

--- front/enjoytrip-front/src/components/user/user-join-form.vue
+++ front/enjoytrip-front/src/components/user/user-join-form.vue
@@ -39,77 +39,64 @@
     <v-text-field
       label="비밀번호"
       v-model="userPassword"
-      type='password'
+      type="password"
       outlined
       dense
     ></v-text-field>
     <v-text-field
       label="비밀번호 확인"
       :rules="passwordRules"
-      type='password'
+      type="password"
       outlined
       dense
     ></v-text-field>
-    <v-btn
-      type="submit"
-      color="primary"
-      width="80%"
-      @click="joinUser"
-      :disabled="invalid"
-    >
+    <v-btn type="submit" color="primary" width="80%" @click="joinUser" :disabled="invalid">
       회원가입
     </v-btn>
-    <v-btn 
-      @click="clear"
-      color="primary"
-      width="20%"
-      outlined
-    >
-      초기화
-    </v-btn>
+    <v-btn @click="clear" color="primary" width="20%" outlined> 초기화 </v-btn>
   </form>
 </template>
 
 <script>
-import { joinUserApi } from '@/api/user';
+import { joinUserApi } from "@/api/user";
 export default {
   name: "UserJoinForm",
   data() {
     return {
       invalid: false,
       userId: "",
-      userNickname:"",
+      userNickname: "",
       userName: "",
       userEmail: "",
-      userPassword: "",    
-      errors: "", 
+      userPassword: "",
+      errors: "",
       passwordRules: [
-        value => !!value || '비밀번호를 입력해주세요.',
-        value => {
-          if (this.userPassword === value)
-            return true;
+        (value) => !!value || "비밀번호를 입력해주세요.",
+        (value) => {
+          if (this.userPassword === value) return true;
           else {
             return "비밀번호가 일치하지 않습니다.";
           }
         },
       ],
       emailRules: [
-        value => !!value || '이메일을 입력해주세요.',
-        value => {
-          const pattern = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-          return pattern.test(value) || '잘못된 이메일 형식입니다.'
+        (value) => !!value || "이메일을 입력해주세요.",
+        (value) => {
+          const pattern =
+            /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+          return pattern.test(value) || "잘못된 이메일 형식입니다.";
         },
       ],
-    }
+    };
   },
   methods: {
     clear() {
-      this.invalid = false,
-      this.userId = "",
-      this.userName = "",
-      this.userEmail = "",
-      this.userPassword = "",     
-      this.errors = ""
+      (this.invalid = false),
+        (this.userId = ""),
+        (this.userName = ""),
+        (this.userEmail = ""),
+        (this.userPassword = ""),
+        (this.errors = "");
     },
     joinUser() {
       let body = {
@@ -122,18 +109,16 @@ export default {
       joinUserApi(
         body,
         () => {
-          alert("회원등록 성공!")
+          alert("회원등록 성공!");
+          this.$router.push("/");
         },
         (error) => {
           console.log(error);
         }
       );
-    }
-
+    },
   },
-}
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
